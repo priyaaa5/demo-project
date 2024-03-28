e6b62ca6d70fb0c02c3c6aa8f52a8382ef664467
priyaaa5 <162120937+priyaaa5@users.noreply.github.com>
2024-03-28 11:28:48 +0530

    Update config.js

--- server/src/model/config.js
+++ server/src/model/config.js
@@ -3,7 +3,7 @@ module.exports = {
         name: 'notes',
         user: 'root',
         password: 'password',
-        host: 'localhost',
+        host: 'dev-db',
         dialect: process.env.APP_ENV === 'test' ? 'sqlite' : 'mysql',
         dialectOptions : process.env.APP_ENV === 'test' ? {} : {
             useUTC:false,
