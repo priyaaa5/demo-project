1cf02137d31f2ccde002512639656b099f25b0ca
priyaaa5 <priya.singh@netsmartz.com>
2024-04-24 07:30:22 +0000

    v

--- notes-ui/src/components/Table.js
+++ notes-ui/src/components/Table.js
@@ -21,7 +21,7 @@ class Table extends Component {
     componentDidMount() {
         this.setState({ isLoading: true })
 
-        let api_url = 'http://52.188.226.205:4006/notes';
+        let api_url = 'http://52.188.226.205:4007/notes';
  
         fetch(api_url)
             .then(res => {
